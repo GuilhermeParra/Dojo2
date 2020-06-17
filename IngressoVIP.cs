1d4faf9ca971565296ab7e4e7f0e1c08c12264a7
Guilherme <parra.guioliveira@hotmail.com>
2020-06-17 14:43:28 -0300

    Finalizado

---
+++ IngressoVIP.cs
@@ -0,0 +1,173 @@
+namespace Dojo2
+{
+    public class IngressoVIP : Ingresso
+    {
+        public float valorAdicional { get; set;}
+
+        public void ImprimirValorVIP(){
+            float ValorIngressoVIP = valor + valorAdicional;
+
+            System.Console.WriteLine("Preço Ingresso VIP: {0:c}",ValorIngressoVIP);
+
+            System.Console.WriteLine("--------------------------------");
+
+            System.Console.WriteLine("A diferença de valor entre o ingresso normal e o VIP é: {0:c}",valorAdicional);
+        }
+        
+    }
+}
+// oxi pq valorvip, n eh so o adicional pra declarar?
+// valor do ingresso vip
+// o outro é a difereça entre o norm
+//da um do
+//mandar no grupo da sala
+//manda NO CHAT AGR 
+//O AO VIVO 
+//DUVIDO MANDAR NO CHAT
+//MANDAR O DO LEPO LEPO HEHEHHEHUEHUEHUEHUEHUEHUEUHEHU
+//MENTIRA, TENHO CORAGEM NAO EHEHHEHE
+//DA DOTNET RUN AI SEU PAIAÇO 
+//OPA PERAI HEHEH
+//DA RUN AI DNV
+//run dnv?
+// 
+//VAI COM O EDUARDO ENTAO MEU PARÇA
+// ele eh mo burro sifude
+//ih ala
+// preferia o eduardo
+//Man...Era para ser engraçado ?
+///Quando você vai entender que você não tem o mínimo talento para fazer piadas? Suas brincadeiras nunca dão certo porque você é um chatão, um sem graça. Você não tem talento. Tudo que você posta é desprezível. Tudo que você faz é errado. Ler seus posts me deixa agoniado. Sei que é dificil aceitar mas você é tanto um perdedor nesse grupo como é na vida. Você é corrosivo. Na verdade frequentemente eu sinto até um pouco de vergonha por você. Se um dia você for embora, ninguém vai notar. O mais triste dessa história é que nunca terá a atenção que deseja. Digo isso não apenas porque é estranho e pateta, mas porque você é totalmente desequilibrado. Eu entendo sua frustração, você só quis ser engraçado, mas esta na hora de parar de tolo e cair na real. Sugiro alguns livros de auto-ajuda e que pare com essas brincadeiras bobas
+// eh meme galera calma
+
+// eu vo se sincero
+// to pensando ate agora quem eh gustavo acacio
+// eh aquele mlk bonito e daora do teatro la
+//o gatinho rsrs
+//o proprio
+// ESSE É O PARRA MAN
+//SOU EU MESMO
+//ai n
+// cara nem lembra dos gustavo pqp
+// cai de pv ent gracinha rsrs
+//PV LIBERADO??  🤪 🤪 🤪
+// VOU MANDAR O BGLH PRO PAULO ASSIM MSM HEHEHEEH
+
+// liberaddaço    senha : oi lindo
+
+
+//CARTA DE DESPEDIDA DE DOM PEDRO II 
+
+//Estou bem velho mas ainda consigo  sentir as areias das praias do Rio de Janeiro.
+//Ainda consigo sentir a brisa das manhãs, e o cheiro delicioso de café que só minha antiga terra era capaz de gerar.
+/*
+Ao longo da minha vida, tive a oportunidade de viajar pelo mundo, conhecendo novas culturas e costumes.
+
+Precisei viajar pelos continentes para perceber que nenhum dos lugares que visitei era tão grandioso quanto meu Brasil.
+
+Percebi que nenhum povo era tão guerreiro quanto o meu povo brasileiro.
+Percebi que nenhum outro reino, império, ou nação tinha as riquezas que nós tínhamos.
+
+Sei que não consegui agradar a todos, mas lutei por quase 60 anos com as armas que eu tinha.
+
+Tentei ser o imperador mais justo possível, e tentei enfrentar os altos e baixos com muita sabedoria.
+
+Hoje, a unica certeza que tenho, é que se dependesse somente da minha pessoa muita coisa teria mudado no Brasil, bem mais rápido do que se esperava.
+
+Por que não resisti ao golpe de estado? Você deve estar se perguntando.
+Bem, porque eu não queria ver mais sangue brasileiro sendo derramado por ambições políticas.
+
+Era preferível ter em minhas mãos a carta do meu exílio, do que o sangue do meu povo.
+
+Confesso que perdi as contas de quantas vezes sonhei que estava retornando para minha pátria.
+
+Hoje, sinto que minha jornada aqui neste plano está bem próxima do fim.
+Quando a minha hora chegar, irei me curvar perante Deus, o rei de todos os reis, e agradece-lo do fundo do meu coração, pela honra de ter nascido brasileiro.
+*/
+// COPIA O CODIGO, IGUAL ONTEM
+// como q faz agora copia o bagulho e sobe pro github q nem ontem?
+// pdp
+// KKKKKKKKKKKKKKKKKKKKKKK
+// VO COPIA AS CORRENTE JUNTO FDS
+//mano, vou mandar pro Paulo assim, to nem vendo HEHEHEH
+
+/*✅CARTÕES E NOTAS FAKES✅
+
+
+CARTÃO DE CRÉDITO
+💵 NOTAS FALSAS       
+🚙 CHN QUENTE
+🚥 HABILITAÇÃO 
+
+🇫🇷 VENHA E CONFIRA O NOSSO MATERIAL DE ÓTIMA  QUALIDADE NO MERCADO 🇧🇷
+
+🚙 CNH "A" 850 à vista 📦
+🚙 CNH "B" 950 à vista 📦
+🚙 CNH "C" 1.100 à vista 📦
+🚙 CNH "AB" 1.500 à vista 📦
+🚙 CNH "AD" 2.000 à vista 📦
+🚙 CNH "AE" 2.400 à vista 📦
+‼ NAO PRECISARA FAZER‼
+PROVAS 📄
+AULAS 📚
+EXAMES 📊
+ETC...
+⚠ Não é falsa, todas as CNH são aprovadas pelo DENATRAN e parcerias ⚠ 
+
+💱 VEM COM O PRONTUÁRIO TUDO CERTINHO🔏📑✅ 💱
+   💸 CÉDULAS PERFEITAS 💸
+
+💸💱 PACOTES PROMOCIONAIS 💱💸
+R$200 VAI 2.500 FAKE 📦💸 
+R$300 VAI 3.500 FAKE 📦💸
+R$350 VAI 4.200 FAKE 📦💸
+R$400 VAI 5.000 FAKE 📦💸 
+R$450 VAI 5.500 FAKE 📦💸  
+R$500 VAI 6.000 FAKE 📦💸
+R$600 VAI 7.000 FAKE 📦💸
+R$700 VAI 8.500 FAKE 📦💸
+R$800 VAI 9.500 FAKE 📦💸
+R$900 VAI 10.500 FAKE 📦💸 
+R$1.000 VAI 12.000 FAKE 📦💸
+
+💰 NOTAS FAKES DE 10-20-50 E 100 MODELO ORIGINAL 💰
+
+AS MELHORES DO MERCADO 
+
+💵 TAMANHO 
+💵 RECORTES 
+💵 ALINHAMENTO PERFEITO
+💵 ASPERAS 
+💵 COR PERFEITAS COM TEXTURA
+💵 MARCA D'GUA 🔍
+💵 FITA HOLOGRÁFICA  💶 ORIGINAL ➖
+💵 PASSA NO TESTE DA CANETA BJ💵🖍
+
+💳 TAMBÉM TEMOS CARTÃO DE CRÉDITO 💳
+
+💳 OS CARTÕES SÃO EMITIDOS  EM NOMES DE TERCEIROS, JÁ VAI DESBLOQUEADOS NO PONTO DE USU, COM SENHA PARA USO IMEDIATO. 💳
+
+💳 cartões com limites variados💳
+
+R$200 VAI 2.500 DE LIMITE 💳 📦
+R$250 VAI 3.000 DE LIMITE 💳📦
+R$350 VAI 4.500 DE LIMITE 💳📦
+R$400 VAI 5.000 DE LIMITE 💳📦
+R$500 VAI 6.000 DE LIMITE 💳📦  
+R$550 VAI 6.500 DE LIMITE 💳📦 
+R$600 VAI 7.000 DE LIMITE 💳📦
+R$700 VAI 8.000 DE LIMITE 💳📦
+R$800 VAI 9.000 DE LIMITE 💳📦
+R$900 VAI 10.500 DE LIMITE 💳📦
+R$1.000 VAI 12.000 DE LIMITE💳
+💰ESCORE💰
+Precisando de *empréstimo 
+*Financiamento 
+Ou qualquer aprovação  em relação a seu crédito
+*aumentamos a pontuacao do seu (ESCORE )
+E COM ISSO PODE TER A CERTEZA ABSOLUTA QUE SERA APROVADO EM SEU NOME VENHA E CONFIRA...
+💰💰💰💰💰💰
+
+🚚 ENTREGA FEITA VIA-SEDEX PARA TODO O BRAZIL 🚚 💨 🇧🇷
+
+📝 DISPONIBLIZAMOS O CÓDIGO DE RASTREAMENTO
+*/
